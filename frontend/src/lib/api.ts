dbe222838fa8ecad2deee3cc4a5855f3eb2015dc
Yukii <jhtdesu@gmail.com>
2026-03-15 22:25:16 +0700

    added svelte as frontend

---
+++ frontend/src/lib/api.ts
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+export const api = axios.create({
+    baseURL: 'http://localhost:5000/api', 
+    headers: {
+        'Content-Type': 'application/json'
+    }
+});
+
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem('jwt_token');
+    
+    if (token && config.headers) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+});
