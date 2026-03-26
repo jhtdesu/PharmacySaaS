36fec0d6865a08684912aa79649c628f12a586fa
Yukii <jhtdesu@gmail.com>
2026-03-26 09:19:26 +0700

    added navbar

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,9 +1,23 @@
 <script lang="ts">
 	import './layout.css';
+	import { page } from '$app/state';
 	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<header class="flex gap-4 bg-green-900 p-2 ">
+	<a class = "font-bold text-emerald-300 hover:text-emerald-500" href="/homepage" class:active={page.url.pathname.startsWith('/homepage')}>Home</a>
+	<a class = "font-bold text-emerald-300 hover:text-emerald-500" href="/dashboard" class:active={page.url.pathname.startsWith('/dashboard')}>Dashboard</a>
+	<a class = "font-bold text-emerald-300 hover:text-emerald-500" href="/medicines" class:active={page.url.pathname.startsWith('/medicines')}>Medicines</a>
+	<a class = "font-bold text-emerald-300 hover:text-emerald-500" href="/medicineBatches" class:active={page.url.pathname.startsWith('/medicineBatches')}>Batches</a>
+	<a class = "font-bold text-emerald-300 hover:text-emerald-500" href="/auth" class:active={page.url.pathname.startsWith('/auth')}>Login</a>
+</header>
+
+<main class="page-content">
+	{@render children()}
+</main>
